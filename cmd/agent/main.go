7ceb235f3e9e9748ab33b9c2127c881b0caaacb7
Alexey <alexey.chimochikhin@sravni.ru>
2026-08-22 18:51:19 +0300

    Базовая заготовка agent

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,37 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"time"
+
+	"github.com/omcg33/go-metrics/internal/agent"
+)
+
+const (
+	pollInterval   = 2 * time.Second
+	reportInterval = 10 * time.Second
+)
+
+func main() {
+	pollTicker := time.NewTicker(pollInterval)
+	reportTicker := time.NewTicker(reportInterval)
+	collector := agent.NewRuntimeCollector()
+	defer pollTicker.Stop()
+	defer reportTicker.Stop()
+
+
+	for {
+	select {
+	case <-pollTicker.C:
+		collector.Collect()
+	case <-reportTicker.C:
+		report := collector.Report()
+		fmt.Println(report)
+	}
+}
+
+	
+	
+	
+	
+}
