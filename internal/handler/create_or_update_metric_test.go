3a777eab0d29ad759c92b2e19c427a972a599035
Alexey <alexey.chimochikhin@sravni.ru>
2026-08-24 10:00:39 +0300

    Добавил тест на handler

---
+++ internal/handler/create_or_update_metric_test.go
@@ -0,0 +1,72 @@
+package handler
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateOrUpdateMetric_ValidateStructError(t *testing.T) {
+	rr := createOrUpdate(&serviceMock{}, "unknown", "Alloc", "1.5")
+
+	assert.Equal(t, http.StatusBadRequest, rr.Code)
+}
+
+func TestCreateOrUpdateMetric_ValidationErrorsAs(t *testing.T) {
+	rr := createOrUpdate(&serviceMock{}, "gauge", "", "1.5")
+
+	assert.Equal(t, http.StatusNotFound, rr.Code)
+}
+
+func TestCreateOrUpdateMetric_ParseFloatError(t *testing.T) {
+	rr := createOrUpdate(&serviceMock{}, "gauge", "Alloc", "abc")
+
+	assert.Equal(t, http.StatusBadRequest, rr.Code)
+}
+
+func TestCreateOrUpdateMetric_ParseIntError(t *testing.T) {
+	rr := createOrUpdate(&serviceMock{}, "counter", "PollCount", "1.5")
+
+	assert.Equal(t, http.StatusBadRequest, rr.Code)
+}
+
+func TestCreateOrUpdateMetric_CreateOrUpdateGauge(t *testing.T) {
+	svc := &serviceMock{}
+	svc.On("CreateOrUpdateGauge", "Alloc", 1.5).Return()
+
+	createOrUpdate(svc, "gauge", "Alloc", "1.5")
+
+	svc.AssertCalled(t, "CreateOrUpdateGauge", "Alloc", 1.5)
+}
+
+func TestCreateOrUpdateMetric_CreateOrUpdateCounter(t *testing.T) {
+	svc := &serviceMock{}
+	svc.On("CreateOrUpdateCounter", "PollCount", int64(42)).Return()
+
+	createOrUpdate(svc, "counter", "PollCount", "42")
+
+	svc.AssertCalled(t, "CreateOrUpdateCounter", "PollCount", int64(42))
+}
+
+func TestCreateOrUpdateMetric_StatusOK(t *testing.T) {
+	svc := &serviceMock{}
+	svc.On("CreateOrUpdateGauge", "Alloc", 1.5).Return()
+
+	rr := createOrUpdate(svc, "gauge", "Alloc", "1.5")
+
+	assert.Equal(t, http.StatusOK, rr.Code)
+}
+
+func createOrUpdate(svc *serviceMock, metricType, name, value string) *httptest.ResponseRecorder {
+	rr := httptest.NewRecorder()
+	req := httptest.NewRequest(http.MethodPost, "/", nil)
+	req.SetPathValue("type", metricType)
+	req.SetPathValue("name", name)
+	req.SetPathValue("value", value)
+
+	NewController(svc).CreateOrUpdateMetric(rr, req)
+
+	return rr
+}
