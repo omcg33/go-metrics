0569857908cfbae1dedc7d09b063687244ef5ca6
Alexey <alexey.chimochikhin@sravni.ru>
2026-08-27 20:24:49 +0300

    Реализовал проброс параметров в агента

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,21 +1,32 @@
 package main
 
 import (
+	"flag"
 	"time"
 
 	"github.com/omcg33/go-metrics/internal/agent"
 )
 
-const (
-	pollInterval   = 2 * time.Second
-	reportInterval = 10 * time.Second
+var (
+	serverAddress *string
+	reportInterval *int
+	pollInterval *int
 )
 
+func init() {
+  serverAddress = flag.String("a", "localhost:8080", "Адрес сервера")
+  reportInterval = flag.Int("r", 10 , "Частота отправки метрик на сервер")
+  pollInterval = flag.Int("p", 2 , "Частота опроса метрик из пакета ")
+}
+
+
 func main() {
-	pollTicker := time.NewTicker(pollInterval)
-	reportTicker := time.NewTicker(reportInterval)
+	flag.Parse();
+
+	reportTicker := time.NewTicker(time.Duration(*reportInterval) * time.Second)
+	pollTicker := time.NewTicker(time.Duration(*pollInterval) * time.Second)
 	collector := agent.NewRuntimeCollector()
-	service := agent.NewService()
+	service := agent.NewService(*serverAddress)
 
 	defer pollTicker.Stop()
 	defer reportTicker.Stop()
